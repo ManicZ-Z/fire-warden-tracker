4ce8b28b64ba34f3ac1cc2a9e051c5830401a133
will <willako316@gmail.com>
2026-01-12 02:59:53 +0000

    Fix Express 5 catch-all route for production

--- server/index.js
+++ server/index.js
@@ -558,7 +558,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(buildPath));
 
   // React Router support: all non-/api routes return index.html
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 } else {
